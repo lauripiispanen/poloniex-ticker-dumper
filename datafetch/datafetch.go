639f36761c3132cf28fa42ffa7d9b0b236833431
Lauri Piispanen <lauri@piispanen.fi>
2017-09-22 23:15:59 +0300

    Initial commit

---
+++ datafetch/datafetch.go
@@ -0,0 +1,79 @@
+package datafetch
+
+import (
+  "net/http"
+  "encoding/json"
+  "time"
+  "io/ioutil"
+  "errors"
+)
+
+func Fetch() ([]TickerDatum, error) {
+  bodyBytes, err := FetchData(&http.Client {})
+  if err != nil {
+    return nil, err
+  }
+  return ParseResponse(bodyBytes)
+}
+
+func FetchData(getter Getter) ([]byte, error) {
+  resp, err := getter.Get("https://poloniex.com/public?command=returnTicker")
+  if err != nil {
+    return nil, err
+  }
+  defer resp.Body.Close()
+  if resp.StatusCode == 200 {
+    bodyBytes, err := ioutil.ReadAll(resp.Body)
+    return bodyBytes, err
+  } else {
+    return nil, errors.New("Got non-200 status")
+  }
+}
+
+func ParseResponse(input []byte) ([]TickerDatum, error) {
+  var parsed map[string]TickerData
+  var out []TickerDatum
+  err := json.Unmarshal(input, &parsed)
+  if err != nil {
+    return nil, err
+  }
+  timestamp := time.Now().Unix()
+
+  for key, value := range parsed {
+    datum := TickerDatum {Timestamp: timestamp, CurrencyPair: key, Last: value.Last, LowestAsk: value.LowestAsk, HighestBid: value.HighestBid, PercentChange: value.PercentChange, BaseVolume: value.BaseVolume, QuoteVolume: value.QuoteVolume, IsFrozen: value.IsFrozen, High24hr: value.High24hr, Low24hr: value.Low24hr }
+    out = append(out, datum)
+  }
+  return out, nil
+}
+
+
+type Getter interface {
+  Get(url string) (resp *http.Response, err error)
+}
+
+type TickerData struct {
+  Id            int
+  Last          string
+  LowestAsk     string
+  HighestBid    string
+  PercentChange string
+  BaseVolume    string
+  QuoteVolume   string
+  IsFrozen      string
+  High24hr      string
+  Low24hr       string
+}
+
+type TickerDatum struct {
+  Timestamp     int64
+  CurrencyPair  string
+  Last          string
+  LowestAsk     string
+  HighestBid    string
+  PercentChange string
+  BaseVolume    string
+  QuoteVolume   string
+  IsFrozen      string
+  High24hr      string
+  Low24hr       string
+}
---
+++ datafetch/datafetch_test.go
@@ -0,0 +1,68 @@
+package datafetch
+
+import (
+  "testing"
+  "net/http"
+  "io"
+  "bytes"
+)
+
+func TestParsing(t *testing.T) {
+  str := []byte("{\"BTC_BCN\":{\"id\":7,\"last\":\"0.00000031\",\"lowestAsk\":\"0.00000031\",\"highestBid\":\"0.00000030\",\"percentChange\":\"-0.03125000\",\"baseVolume\":\"40.22111561\",\"quoteVolume\":\"129435889.20216185\",\"isFrozen\":\"0\",\"high24hr\":\"0.00000032\",\"low24hr\":\"0.00000030\"},\"BTC_BELA\":{\"id\":8,\"last\":\"0.00003598\",\"lowestAsk\":\"0.00003599\",\"highestBid\":\"0.00003598\",\"percentChange\":\"0.16628849\",\"baseVolume\":\"29.74189120\",\"quoteVolume\":\"908673.62756949\",\"isFrozen\":\"0\",\"high24hr\":\"0.00003747\",\"low24hr\":\"0.00003000\"},\"BTC_BLK\":{\"id\":10,\"last\":\"0.00004560\",\"lowestAsk\":\"0.00004545\",\"highestBid\":\"0.00004519\",\"percentChange\":\"-0.07990314\",\"baseVolume\":\"40.01428327\",\"quoteVolume\":\"836047.58947475\",\"isFrozen\":\"0\",\"high24hr\":\"0.00005217\",\"low24hr\":\"0.00004327\"}}")
+  tickers, err := ParseResponse(str)
+  if err != nil {
+    t.Fatalf("Expected to have no errors but got: %+v", err)
+  }
+  if len(tickers) != 3 {
+    t.Fatalf("Expected to have three tickers but had: %+v", len(tickers))
+  }
+  var BTC_BCN *TickerDatum
+  for i := range tickers {
+    var ticker = &tickers[i]
+    if ticker.CurrencyPair == "BTC_BCN" {
+      BTC_BCN = ticker
+    }
+  }
+  if BTC_BCN == nil {
+    t.Fatal("Expected to find BTC_BCN ticker, but didn't")
+  }
+  if BTC_BCN.Last != "0.00000031" {
+    t.Fatal("Expected to have correct value for BTC_BCN.Last, but got:", BTC_BCN.Last)
+  }
+}
+
+func TestFetchDataWithErrorStatusCode(t *testing.T) {
+  _, err := FetchData(GetterMock{ get: func (url string) (*http.Response, error) {
+    return &http.Response { Body: ResponseBody(""), StatusCode: 400 },nil
+  }})
+  if err == nil {
+    t.Fatal("Expected to get error")
+  }
+}
+
+func TestFetchDataWithBody(t *testing.T) {
+  bodyBytes, err := FetchData(GetterMock{ get: func (url string) (*http.Response, error) {
+    return &http.Response { Body: ResponseBody("foo"), StatusCode: 200 },nil
+  }})
+  if err != nil {
+    t.Fatal("Shouldn't have errors")
+  } else if !bytes.Equal(bodyBytes, []byte("foo")) {
+    t.Fatal("Body not passed correctly")
+  }
+}
+
+type GetterMock struct {
+  get func(string) (*http.Response, error)
+}
+
+func (g GetterMock) Get(url string) (*http.Response, error) { return g.get(url) }
+
+type nopCloser struct {
+    io.Reader
+}
+
+func (nopCloser) Close() error { return nil }
+
+func ResponseBody(str string) io.ReadCloser {
+  return nopCloser{bytes.NewBufferString(str)}
+}
